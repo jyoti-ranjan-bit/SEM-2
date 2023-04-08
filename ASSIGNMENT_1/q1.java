9ea1b3b55129b7c7780173e0b7335996fd0c18cf
codex_jrp <118820439+jyoti-ranjan-bit@users.noreply.github.com>
2023-04-09 01:16:44 +0530

    Update q1.java

--- ASSIGNMENT_1/q1.java
+++ ASSIGNMENT_1/q1.java
@@ -2,16 +2,13 @@ public class q1 {
 
 	public static void main(String[] args) {
 		int n=Integer.parseInt(args[0]);
-		int count=0;int g=1;
-		for(int i=1;i<n;i++) {
-			g=g*2;
+		int count=0;
+		while (n>2) {
 			count++;
-			if(g>n) {
-				break;
-			}	
+			n=n/2;
 		}
-		count=count-2;
-		System.out.println("total no of time is "+ count);
+		
+		System.out.println("total no of time one must repeatedly divide this number by 2 before getting a value less than 2 is "+ count);
 	}
 
 }
