678727406bc140a68596b2ce459d4859676b9006
codex_jrp <118820439+jyoti-ranjan-bit@users.noreply.github.com>
2023-04-11 19:03:13 +0530

    Update q4.java

--- ASSIGNMENT_1/q4.java
+++ ASSIGNMENT_1/q4.java
@@ -1,3 +1,15 @@
+/*Question-4:
+Write a Java program that outputs all possible strings formed by using the characters 'c', 'a', 'r',
+'b', 'o', and 'n' exactly once.
+Example:
+For 3 characters ‘C’, ‘A’, ‘R’, the possible strings are
+CAR
+CRA
+RCA
+RAC
+ACR
+ARC*/
+
 import java.util.Scanner;
 public class q4 {
 
