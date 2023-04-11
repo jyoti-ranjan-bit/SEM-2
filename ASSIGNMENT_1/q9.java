0c047d235b91a2127401a37cffb512ac37d94ae8
codex_jrp <118820439+jyoti-ranjan-bit@users.noreply.github.com>
2023-04-11 19:08:15 +0530

    Update q9.java

--- ASSIGNMENT_1/q9.java
+++ ASSIGNMENT_1/q9.java
@@ -1,3 +1,16 @@
+/*Question-9:
+Write a method that sums all the numbers in the major diagonal in an n * n matrix of double
+values using the following header:
+public static double sumMajorDiagonal(double[][] m)
+Write a java program that reads a 4-by-4 matrix and displays the sum of all its elements on the
+major diagonal.
+Sample Run:
+Enter a 4-by-4 matrix row by row:
+1 2 3 4.0
+5 6.5 7 8
+9 10 11 12
+13 14 15 16
+Sum of the elements in the major diagonal is 34.5*/
 import java.util.*;
 public class q9 {
 	public static double sumMajorDiagonal(double[][]m) {
