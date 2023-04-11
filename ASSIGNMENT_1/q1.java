b763117f12668b2d261862ce0a53af9f38f1787d
codex_jrp <118820439+jyoti-ranjan-bit@users.noreply.github.com>
2023-04-11 19:00:00 +0530

    Update q1.java

--- ASSIGNMENT_1/q1.java
+++ ASSIGNMENT_1/q1.java
@@ -1,3 +1,11 @@
+/*Write a Java program that can take a positive integer greater than 2 as input and write out the
+number of times one must repeatedly divide this number by 2 before getting a value less
+than 2.
+Sample Run:
+The positive integer greater than 2 from command line argument
+is 67.
+The number of times one must repeatedly divide this number by 2
+before getting a value less than 2 is 5.*/
 public class q1 {
 
 	public static void main(String[] args) {
