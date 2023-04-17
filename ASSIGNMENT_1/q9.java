c8efddbbee4167e7fa8159d4d796bcc789136dfb
Jyoti Ranjan Padhi <118820439+jyoti-ranjan-bit@users.noreply.github.com>
2023-04-17 19:20:11 +0530

    Update q9.java

--- ASSIGNMENT_1/q9.java
+++ ASSIGNMENT_1/q9.java
@@ -11,42 +11,42 @@ Enter a 4-by-4 matrix row by row:
 9 10 11 12
 13 14 15 16
 Sum of the elements in the major diagonal is 34.5*/
-import java.util.*;
-public class q9 {
-	public static double sumMajorDiagonal(double[][]m) {
-		Scanner sc=new Scanner(System.in);
-		int n=4;
-		int t=4;
-		double sum=0;
-		double arr[][]=new double[n][t];
-		for (int i=0;i<n;i++) {
-			for (int j=0;j<t;j++) {
-				System.out.println("enter element:");
-				double q=sc.nextDouble();
-				arr[i][j]=q;
-				sum=sum+q;
-			}
-		}
-		int c=0;
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<t;j++) {
-				System.out.println(arr[i][j]+"");
-				c++;
-				if(c==4) {
-					System.err.println();
-					c=0;
-				}
-			}
-		}
-		return sum;
-	}
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		double[][]arr=new double[4][4];
-		double s=sumMajorDiagonal(arr);
-		System.out.println(s);
-		
+public class q9{
+public static double sumMajorDiagonal(double[][]m){
 
-	}
-
+double sum=0;
+    for (int i=0;i<4;i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i == j) {
+                sum = sum + m[i][j];
+            }
+        }
+    }
+    return sum;
 }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double arr[][]=new double[4][4];
+        System.out.println("enter a 4x4 matrix by row:");
+        for (int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                double n= sc.nextDouble();
+                arr[i][j]=n;
+            }
+        }
+        int count=0;
+        for (int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                count++;
+                System.out.print(arr[i][j]+" ");
+                if(count==4){
+                    System.out.println();
+                    count=0;
+                }
+            }}
+        System.out.println(sumMajorDiagonal(arr));
+
+    }}
